ae392f6e0788a73462071e9f0e7cd2b7a9283e60
Josh Mack <mack.joshuaj@gmail.com>
2019-09-18 11:56:09 -0500

    Stateless Functional Component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,71 @@
-import React from 'react';
 import NavBar from './components/navbar';
 import './App.css';
 import Counters from "./components/counters";
+import React, { Component } from 'react';
 
-function App() {
+class App extends Component {
+  state = { 
+        counters: [
+            {id: 1, value: 4 },
+            {id: 2, value: 0 },
+            {id: 3, value: 0 },
+            {id: 4, value: 0 }
+        ]
+     }
+
+    handleDelete = (counterId) => {
+        console.log('Event Handler Called', counterId);
+        const counters = this.state.counters.filter( c => c.id !== counterId)
+        this.setState({ counters });
+    };
+
+    handleReset = () => {
+        const counters = this.state.counters.map(c => {
+            c.value = 0;
+            return c;
+        });
+        this.setState({ counters })
+    };
+
+    handleIncrement = counter => {
+        console.log(counter)
+        const counters = [...this.state.counters];
+        const index = counters.indexOf(counter);
+        counters[index] = {...counter};
+        counters[index].value++;
+        this.setState({ counters })
+    }
+   
+    handleDecrement = counter => {
+        console.log(counter)
+        const counters = [...this.state.counters];
+        const index = counters.indexOf(counter);
+        counters[index] = {...counter};
+        if (counters[index].value > 0) {
+            counters[index].value--;
+            this.setState({ counters })
+        } else {
+            console.log("Cannot decrement past 0!")
+        }
+      
+    }
+render(){
   return (
-    <React.Fragment>
-      <NavBar />
+    <div>
+      <NavBar totalCounters={this.state.counters.filter(c => c.value >0).length} />
         <main className="container">
-          <Counters />
+          <Counters 
+            counters={this.state.counters}
+            onReset={this.handleReset} 
+            onIncrement={this.handleIncrement} 
+            onDecrement={this.handleDecrement} 
+            onDelete={this.handleDelete} 
+            />
         </main>
-    </React.Fragment>
+    </div>
   );
+}
+
 }
 
 export default App;
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,65 +2,21 @@ import React, { Component } from 'react';
 import Counter from './counter';
 
 class Counters extends Component {
-    state = { 
-        counters: [
-            {id: 1, value: 4 },
-            {id: 2, value: 0 },
-            {id: 3, value: 0 },
-            {id: 4, value: 0 }
-        ]
-     }
-
-    handleDelete = (counterId) => {
-        console.log('Event Handler Called', counterId);
-        const counters = this.state.counters.filter( c => c.id !== counterId)
-        this.setState({ counters });
-    };
-
-    handleReset = () => {
-        const counters = this.state.counters.map(c => {
-            c.value = 0;
-            return c;
-        });
-        this.setState({ counters })
-    };
-
-    handleIncrement = counter => {
-        console.log(counter)
-        const counters = [...this.state.counters];
-        const index = counters.indexOf(counter);
-        counters[index] = {...counter};
-        counters[index].value++;
-        this.setState({ counters })
-    }
+  
    
-    handleDecrement = counter => {
-        console.log(counter)
-        const counters = [...this.state.counters];
-        const index = counters.indexOf(counter);
-        counters[index] = {...counter};
-        if (counters[index].value > 0) {
-            counters[index].value--;
-            this.setState({ counters })
-        } else {
-            console.log("Cannot decrement past 0!")
-        }
-      
-    }
-   
-
     render() { 
+        const {onReset, counters, onIncrement, onDecrement, onDelete} = this.props;
         return ( 
             <div>
                 <button 
-                    onClick={this.handleReset}
+                    onClick={this.props.onReset}
                     className="btn btn-primary btn-sm m-2">Reset</button>
-                { this.state.counters.map(counter => (
+                { counters.map(counter => (
                     <Counter 
                         key={counter.id} 
-                        onDelete={this.handleDelete}
-                        onIncrement={this.handleIncrement}
-                        onDecrement={this.handleDecrement}
+                        onDelete={onDelete}
+                        onIncrement={onIncrement}
+                        onDecrement={onDecrement}
                         counter={counter}
                     />
                 ))}
