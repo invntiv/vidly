735feccc4f5b43c3cbfff2e3de3395639ff86c6b
Josh Mack <mack.joshuaj@gmail.com>
2019-09-24 13:12:59 -0500

    installed/implemented PropTypes

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -68,7 +68,12 @@ class Movies extends Component {
                         <td>{movie.numberInStock}</td>
                         <td>{movie.dailyRentalRate}</td>
                         <td><Like liked={movie.liked} onClick={() => this.handleLike(movie)}/></td>
-                        <td><button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button></td>
+                        <td>
+                            <button 
+                                onClick={() => this.handleDelete(movie)} 
+                                className="btn btn-danger btn-sm">Delete
+                            </button>
+                        </td>
                     </tr>
                     ))}
                 
